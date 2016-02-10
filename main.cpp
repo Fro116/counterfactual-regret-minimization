c05e1a13a54b3da681eb861c8065b8195bf6409b
Kundan Chintamaneni <omnik116@gmail.com>
2016-02-09 19:55:36 -0500

    Made CFR run in parallel.

--- main.cpp
+++ main.cpp
@@ -16,23 +16,7 @@ int main(int argc, char *argv[]) {
   Random rand;
   std::shared_ptr<PayoutSet<std::string, KuhnPokerInformationSet>> game(new KuhnPokerPayoutSet);
   CounterFactualRegretMinimizer<std::string, KuhnPokerInformationSet> trainer(game);
-  trainer.train(100000);
+  trainer.train(100000,2);
   trainer.saveStrategy("tmp");
-//   KuhnPokerPayoutSet hi;
-//   hi.beginGame();
-//   while (!hi.isTerminalState()) {
-//   std::cout << "PLAYER: " << hi.playerToAct() << std::endl;
-//   std::cout << "OPTIONS: " << std::endl;
-//   for (std::string name : hi.actions()) {
-//     std::cout << name << std::endl;
-//   }
-//   std::string option = hi.actions()[rand.integer(0, hi.actions().size()-1)];
-//   hi.makeMove(option);
-//   std::cout << "CHOICE: " << option << std::endl;
-//   }
-//   std::cout << "VALUE: " << hi.payout()[0] << " " << hi.payout()[1] << std::endl;
-//   std::shared_ptr<PayoutMatrix> payout(new TicTacToePayoutMatrix);
-//   RegretMinimizer trainer(payout);
-//   trainer.train(1000000);
   return 0;
 }
