abff0546a19d79e514c055bdb87b5ea52f9c077c
Kundan Chintamaneni <omnik116@gmail.com>
2016-06-08 13:28:19 -0400

    Implemented test class for parallelization.

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@
 #include <sstream>
 #include <memory>
 
-#include "TicTacToePayoutMatrix.h"
-#include "RegretMinimizer.h"
 #include "KuhnPokerPayoutSet.h"
+#include "ParallelTestPayoutSet.h"
 #include "Random.h"
 #include "CounterFactualRegretMinimizer.h"
 
 int main(int argc, char *argv[]) {
   Random rand;
-  std::shared_ptr<PayoutSet<std::string, KuhnPokerInformationSet>> game(new KuhnPokerPayoutSet);
-  CounterFactualRegretMinimizer<std::string, KuhnPokerInformationSet> trainer(game);
-  trainer.train(100000,2);
+  //  std::shared_ptr<PayoutSet<std::string, KuhnPokerInformationSet>> game(new KuhnPokerPayoutSet);
+  std::shared_ptr<PayoutSet<std::string, ParallelTestInformationSet>> game(new ParallelTestPayoutSet);
+  CounterFactualRegretMinimizer<std::string, ParallelTestInformationSet> trainer(game,true);
+  trainer.train(100000,4);
   trainer.saveStrategy("tmp");
   return 0;
 }
