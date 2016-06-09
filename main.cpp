dabd7f3e88163a7b47caad4860ee04322fe8d6d1
Kundan Chintamaneni <omnik116@gmail.com>
2016-06-08 22:34:08 -0400

    Better parallel implementation.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -5,18 +6,34 @@
 #include <algorithm>
 #include <sstream>
 #include <memory>
+#include <thread>
 
 #include "KuhnPokerPayoutSet.h"
 #include "ParallelTestPayoutSet.h"
 #include "Random.h"
 #include "CounterFactualRegretMinimizer.h"
 
+void train() {
+  std::shared_ptr<PayoutSet<std::string, KuhnPokerInformationSet>> game(new KuhnPokerPayoutSet);
+  CounterFactualRegretMinimizer<std::string, KuhnPokerInformationSet> trainer(game, false);
+  trainer.train(25000,1);
+}
+
 int main(int argc, char *argv[]) {
-  Random rand;
-  //  std::shared_ptr<PayoutSet<std::string, KuhnPokerInformationSet>> game(new KuhnPokerPayoutSet);
-  std::shared_ptr<PayoutSet<std::string, ParallelTestInformationSet>> game(new ParallelTestPayoutSet);
-  CounterFactualRegretMinimizer<std::string, ParallelTestInformationSet> trainer(game,true);
-  trainer.train(100000,4);
-  trainer.saveStrategy("tmp");
+  std::thread t1(train);
+  std::thread t2(train);
+  std::thread t3(train);
+  std::thread t4(train);
+  t1.join();
+  t2.join();
+  t3.join();
+  t4.join();
+
+  //std::shared_ptr<PayoutSet<std::string, ParallelTestInformationSet>> game(new ParallelTestPayoutSet);
+  //CounterFactualRegretMinimizer<std::string, ParallelTestInformationSet> trainer(game,true);
+
+  //  trainer.load("tmp");
+  //  trainer.save("tmp");
+
   return 0;
 }
