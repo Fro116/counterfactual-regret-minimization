311f54e57894dec6a8aad65d097ab37f95ae4261
Fro116 <omnik116@gmail.com>
2016-06-10 21:01:14 -0400

    Separated saving data from checking covergence.

--- main.cpp
+++ main.cpp
@@ -13,20 +13,21 @@
 #include "CounterFactualRegretMinimizer.h"
 
 void train() {
-  std::shared_ptr<PayoutSet<std::string, KuhnPokerInformationSet>> game(new KuhnPokerPayoutSet);
-  CounterFactualRegretMinimizer<std::string, KuhnPokerInformationSet> trainer(game);
-  // trainer.load("tmp.txt");
-  trainer.train("results.txt", 2, 0.01, 10000, 1000);
-  //trainer.train(10000000, 1);
-  //trainer.save("tmp.txt");
+  // std::shared_ptr<PayoutSet<std::string, KuhnPokerInformationSet>> game(new KuhnPokerPayoutSet);
+  // CounterFactualRegretMinimizer<std::string, KuhnPokerInformationSet> trainer(game);
+  //trainer.load("tmp.txt");
+  std::shared_ptr<PayoutSet<std::string, ParallelTestInformationSet>> game(new ParallelTestPayoutSet);
+  CounterFactualRegretMinimizer<std::string, ParallelTestInformationSet> trainer(game);
+  trainer.train("results.txt", 2, 0.1, 100000, 10000, 1000);
+  // for (int i = 0; i < 2; ++i) {
+  //   std::cout << "ITERATION " << i << std::endl;
+  //   trainer.train(10000, 1);
+  // }
+  // trainer.save("tmp.txt");
 }
 
 int main(int argc, char *argv[]) {
   train();
-
-  //std::shared_ptr<PayoutSet<std::string, ParallelTestInformationSet>> game(new ParallelTestPayoutSet);
-  //CounterFactualRegretMinimizer<std::string, ParallelTestInformationSet> trainer(game,true);
-
   //  trainer.load("tmp");
   //  trainer.save("tmp");
 
